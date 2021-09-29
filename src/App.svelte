99c5e4a6c375df17c9e4c30a0fb8153b5711def4
godcomplex26 <godcomplex26@gmail.com>
2021-09-29 16:53:46 +0900

    카카오페이수정

--- src/App.svelte
+++ src/App.svelte
@@ -63,6 +63,11 @@
 			margin-top: 5px;
 			color: #fff;
 		}
+
+		#mySwiper .swiper-pagination-bullet-active {
+		  color: #000;
+		  background: rgba(255, 212, 0, 0.9);
+		}
 	</style>
 	<Swiper direction="{'vertical'}" slidesPerView="{'auto'}" spaceBetween="{0}" freeMode="{true}" mousewheel="{true}" pagination='{{
 		  "clickable": true,
@@ -86,10 +91,8 @@
 			    ]
 			  }
 			}}' id="main_swiper"> -->
-			<Swiper spaceBetween="{30}" effect="{'fade'}" navigation="{true}" pagination='{{
-			  "clickable": true
-			}}' autoplay='{{
-				  "delay": 2500,
+			<Swiper spaceBetween="{30}" speed="{2500}" effect="{'fade'}" navigation="{true}" autoplay='{{
+				  "delay": 3000,
 				  "disableOnInteraction": false
 				}}' id="main_swiper" name="main_swiper">
 				<SwiperSlide id="photo_1"></SwiperSlide>
@@ -104,93 +107,115 @@
 				<p class="message">더 이상 싸우지 않고</p>
 				<p class="message">잘 지낼 수 있도록</p>
 				<p class="message">많은 관심과 사랑 부탁드립니다.</p>
-				<p class="foot">2021.11.14 14:20</p>
+				<p class="foot_number">2021.11.14 14:20</p>
 				<p class="foot">더 베네치아</p>
 				<p class="foot">잠실</p>
 			</div>
 		</SwiperSlide>
-		<SwiperSlide>
-			<Swiper id="photo_album" name="photo_album" class="mySwiper2 swiper-v" preloadImages="{true}" lazy="{true}" slidesPerView="{3}" spaceBetween="{-6}" pagination='{pagination}' autoplay='{{
-				  "delay": 2500,
-				  "disableOnInteraction": false
-				}}'>
-				<SwiperSlide>
-					<div class="image-grid">
-						<img src="img/album/1.jpg" alt="" class="swiper-lazy">
-						<img src="img/album/2.jpg" alt="" class="swiper-lazy">
-						<img src="img/album/3.jpg" alt="" class="swiper-lazy">
-						<img src="img/album/4.jpg" alt="" class="swiper-lazy">
-						<img src="img/album/5.jpg" alt="" class="swiper-lazy">
-					</div>
-				</SwiperSlide>
-				<SwiperSlide>
-					<div class="image-grid">
-						<img src="img/album/6.jpg" alt="" class="swiper-lazy">
-						<img src="img/album/7.jpg" alt="" class="swiper-lazy">
-						<img src="img/album/8.jpg" alt="" class="swiper-lazy">
-						<img src="img/album/9.jpg" alt="" class="swiper-lazy">
-						<img src="img/album/10.jpg" alt="" class="swiper-lazy">
-					</div>
-				</SwiperSlide>
-				<SwiperSlide>
-					<div class="image-grid">
-						<img src="img/album/11.jpg" alt="" class="swiper-lazy">
-						<img src="img/album/12.jpg" alt="" class="swiper-lazy">
-						<img src="img/album/13.jpg" alt="" class="swiper-lazy">
-						<img src="img/album/14.jpg" alt="" class="swiper-lazy">
-						<img src="img/album/15.jpg" alt="" class="swiper-lazy">
-					</div>
-				</SwiperSlide>
-				<SwiperSlide>
-					<div class="image-grid">
-						<img src="img/album/16.jpg" alt="" class="swiper-lazy">
-						<img src="img/album/17.jpg" alt="" class="swiper-lazy">
-						<img src="img/album/18.jpg" alt="" class="swiper-lazy">
-						<img src="img/album/19.jpg" alt="" class="swiper-lazy">
-						<img src="img/album/20.jpg" alt="" class="swiper-lazy">
-					</div>
-				</SwiperSlide>
-				<SwiperSlide>
-					<div class="image-grid">
-						<img src="img/album/21.jpg" alt="" class="swiper-lazy">
-						<img src="img/album/22.jpg" alt="" class="swiper-lazy">
-						<img src="img/album/23.jpg" alt="" class="swiper-lazy">
-						<img src="img/album/24.jpg" alt="" class="swiper-lazy">
-						<img src="img/album/25.jpg" alt="" class="swiper-lazy">
-					</div>
-				</SwiperSlide>
-				<SwiperSlide>
-					<div class="image-grid">
-						<img src="img/album/26.jpg" alt="" class="swiper-lazy">
-						<img src="img/album/27.jpg" alt="" class="swiper-lazy">
-						<img src="img/album/28.jpg" alt="" class="swiper-lazy">
-						<img src="img/album/29.jpg" alt="" class="swiper-lazy">
-						<img src="img/album/30.jpg" alt="" class="swiper-lazy">
-					</div>
-				</SwiperSlide>
-			</Swiper>
+		<SwiperSlide class="slide_with_title">
+			<div class="slide_container">
+				<div class="title_space"><p>History</p></div>
+				<Swiper id="photo_album" name="photo_album" class="mySwiper2 swiper-v" preloadImages="{true}" lazy="{true}" speed="{2000}" slidesPerView="{3}" spaceBetween="{-6}" pagination='{pagination}' autoplay='{{
+					  "delay": 3000,
+					  "disableOnInteraction": false
+					}}'>
+					<SwiperSlide>
+						<div class="image-grid">
+							<img src="img/album/1.jpg" alt="" class="swiper-lazy">
+							<img src="img/album/2.jpg" alt="" class="swiper-lazy">
+							<img src="img/album/3.jpg" alt="" class="swiper-lazy">
+							<img src="img/album/4.jpg" alt="" class="swiper-lazy">
+							<img src="img/album/5.jpg" alt="" class="swiper-lazy">
+						</div>
+					</SwiperSlide>
+					<SwiperSlide>
+						<div class="image-grid">
+							<img src="img/album/6.jpg" alt="" class="swiper-lazy">
+							<img src="img/album/7.jpg" alt="" class="swiper-lazy">
+							<img src="img/album/8.jpg" alt="" class="swiper-lazy">
+							<img src="img/album/9.jpg" alt="" class="swiper-lazy">
+							<img src="img/album/10.jpg" alt="" class="swiper-lazy">
+						</div>
+					</SwiperSlide>
+					<SwiperSlide>
+						<div class="image-grid">
+							<img src="img/album/11.jpg" alt="" class="swiper-lazy">
+							<img src="img/album/12.jpg" alt="" class="swiper-lazy">
+							<img src="img/album/13.jpg" alt="" class="swiper-lazy">
+							<img src="img/album/14.jpg" alt="" class="swiper-lazy">
+							<img src="img/album/15.jpg" alt="" class="swiper-lazy">
+						</div>
+					</SwiperSlide>
+					<SwiperSlide>
+						<div class="image-grid">
+							<img src="img/album/16.jpg" alt="" class="swiper-lazy">
+							<img src="img/album/17.jpg" alt="" class="swiper-lazy">
+							<img src="img/album/18.jpg" alt="" class="swiper-lazy">
+							<img src="img/album/19.jpg" alt="" class="swiper-lazy">
+							<img src="img/album/20.jpg" alt="" class="swiper-lazy">
+						</div>
+					</SwiperSlide>
+					<SwiperSlide>
+						<div class="image-grid">
+							<img src="img/album/21.jpg" alt="" class="swiper-lazy">
+							<img src="img/album/22.jpg" alt="" class="swiper-lazy">
+							<img src="img/album/23.jpg" alt="" class="swiper-lazy">
+							<img src="img/album/24.jpg" alt="" class="swiper-lazy">
+							<img src="img/album/25.jpg" alt="" class="swiper-lazy">
+						</div>
+					</SwiperSlide>
+					<SwiperSlide>
+						<div class="image-grid">
+							<img src="img/album/26.jpg" alt="" class="swiper-lazy">
+							<img src="img/album/27.jpg" alt="" class="swiper-lazy">
+							<img src="img/album/28.jpg" alt="" class="swiper-lazy">
+							<img src="img/album/29.jpg" alt="" class="swiper-lazy">
+							<img src="img/album/30.jpg" alt="" class="swiper-lazy">
+						</div>
+					</SwiperSlide>
+				</Swiper>
+			</div>
 		</SwiperSlide>
-		<SwiperSlide id="map_page" class="map">
-			<img class="map_img" src="img/map.png" />
-			<p>주소: {name}</p>
-			<input bind:value={name}>
-			<button class="btn btn-primary" on:click={copy}>주소복사하기</button>
-			<div id="clipboard"></div>
+		<SwiperSlide id="map" class="slide_with_title">
+			<div class="slide_container">
+				<div class="title_space"><p>오시는 길</p></div>
+				<img class="map_img" src="img/map.png" />
+				<!-- <p>주소: {name}</p>
+				<input bind:value={name}>
+				<button class="btn btn-primary" on:click={copy}>주소복사하기</button>
+				<div id="clipboard"></div> -->
+			</div>
 		</SwiperSlide>
-		<SwiperSlide class="contact">
-			<div class="grid-container">
-				<div class='grid-item'><p class='contact_title'>마음 전하실 곳</p></div>
-				<div class='grid-item'></div>
-				<div class='grid-item'></div>
-				<div class='grid-item'><a href='https://qr.kakaopay.com/28100601100004868083261861a807333'><img class='kakaopay' src='img/kakaopay.png' /></a></div>
-				<div class='grid-item'><a href='https://qr.kakaopay.com/28100601100004868083261861a807333'><img class='kakaopay' src='img/kakaopay.png' /></a></div>
-				<div class='grid-item'><a href='tel:01073003086'><img class='phone' src='img/phone.jpg'/></a></div>
-				<div class='grid-item'><a href='tel:01073003086'><img class='phone' src='img/phone.jpg'/></a></div>
+		<SwiperSlide id="contact" class="slide_with_title">
+			<div class="slide_container">
+				<div class="title_space"><p>마음 전하실 곳</p></div>
+				<div class="grid-container">
+					<div class='grid-item'>
+						<p class="sorry">코로나로 힘든 시기에 결혼식을</p>
+						<p class="sorry">올리게 되어 죄송스럽습니다.</p>
+						<p id="sorry_newline" class="sorry">혹시 참석하지 못하시더라도</p>
+						<p class="sorry">축복해 주시면 감사하겠습니다.</p>
+					</div>
+					<div class='grid-item'>
+						<h1>신랑</h1>
+						<p>김성진</p>
+						<p>KB 91028884504</p>
+						<a href='https://qr.kakaopay.com/281006011171148890009784'><img class='kakaopay' src='img/카카오페이_CI_combination_with_BG.svg' /></a>
+					</div>
+					<div class='grid-item'>
+						<h1>신부</h1>
+						<p>김유림</p>
+						<p>SC제일 38520100690</p>
+						<a href='https://qr.kakaopay.com/281006011000071963774752'><img class='kakaopay' src='img/카카오페이_CI_combination_with_BG.svg' /></a>
+					</div>
+					<div class='grid-item'><a href='tel:01073003086'><img class='phone' src='img/phone.jpg'/></a></div>
+					<div class='grid-item'><a href='tel:01073003086'><img class='phone' src='img/phone.jpg'/></a></div>
+				</div>
 			</div>
 		</SwiperSlide>
 	</Swiper>
 
-	<script>
+	<!-- <script>
 
 		var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
 		    mapOption = { 
@@ -222,7 +247,7 @@
 
 		// 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
 		infowindow.open(map, marker);
-	</script>
+	</script> -->
 </main>
 
 <style>
@@ -234,12 +259,12 @@
 		/*margin-top: 60px;*/
 	}
 
-	h1 {
+	/*h1 {
 		color: #ff3e00;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
-	}
+	}*/
 
 	/*@media (min-width: 640px) {
 		main {
